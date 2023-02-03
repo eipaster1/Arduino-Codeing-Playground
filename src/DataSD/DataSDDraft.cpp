fe7f12e27035e65c290418935adf07ef78705995
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-02-03 13:30:23 -0500

    Woring on making structure to print

--- src/DataSD/DataSDDraft.cpp
+++ src/DataSD/DataSDDraft.cpp
@@ -2,34 +2,35 @@
 #include <SD.h>
 #include <SPI.h>
 #include <Arduino.h>
-
+#define SD_CHIP_SELECT_PIN 4
 
 /*Introducing variables for analyzing the analog input*/
 
 //OD Placehold for single analog read 
-int OD;
+int V;
 //Analog Tag for OD
-int ReadOD = A0;
+int ReadV = A0;
 //Amount of Readings before taking an average
 int numReadings;
 //Analog number recorded onto the SD
-int ODRecord;
+int VRecord;
 //Readings Counter Goal
 int numReadTar =5 ;
 //Readings Counter
 int numRead;
 //File indentifier
-File file;
-/*
-For: https://forum.arduino.cc/t/sd-examples-and-chip-select-definition/126821/3
+File myfile;
 
-The following notes were made:
+//Now making a structure call writer
 
-For the Ethernet shield, pin 10 is chip select for the Ethernet controller
- and pin 4 is chip select for the SD card. So you should make pin 10 an output
-  and set it high to disable the Ethernet controller, not because pin 10 is SS.
-*/
+struct Datastore{
+unsigned int AnalogV;
+};
 
+//Making a file name
+
+
+const char filename = "System_State_Log.dat";
 
 
 
@@ -39,51 +40,68 @@ void setup(){
 
 pinMode(A0,INPUT);
 
-//SD Begin Input as 4 which is defult
-
-// SS must be in output mode even it is not chip select
-//pinMode(SS_PIN, OUTPUT); **
-//digitalWrite(SS_PIN, HIGH); **
-
+//Setting specific structure
 
 
 //Starting serial terminal 
 
 Serial.begin(9600);
-ODRecord = 0;
+SD.begin(SD_CHIP_SELECT_PIN);
+
+//checking if the pin is actually being Read
+if(!SD.begin(SD_CHIP_SELECT_PIN)){
+Serial.println("initialization failed!");
+        return;
+}
+
+//Now setting specific structure
+const char datalen = 10;//Number of entries in structure
+Datastore writer[datalen/* Arguement of how big structure is*/];
+
+
+
+
+
+
+
+
+myfile = SD.open(filename,FILE_WRITE);
+
+VRecord = 0;
 numRead = 0;
-OD = 0;
-SD.open("System_State_Log.txt",FILE_WRITE);
-
-
+V = 0;
 }
 
 
 
-void loop(){
+void loop() {
 //After Recording Value onto SD the num Reader goes back to zero
 
 
-OD = analogRead(ReadOD);
-ODRecord += OD;
+V = analogRead(ReadV);
+VRecord += V;
 numRead += 1;
 
 //Now using if statement to determine if the number of readings were met
 
+
 if(numRead == numReadTar){
-ODRecord = ODRecord/numRead;
-Serial.print("Analog OD Value: ");
-Serial.println(ODRecord);
-//Recording OD Analog to Record
-SD.begin(4);
-SD.open("System_State_Log.txt",FILE_WRITE);
-file.print(ODRecord);
-file.close();
+  VRecord = VRecord/numRead;
+  Serial.print("Analog Value: ");
+  Serial.println(VRecord);
+//Recording V Analog to Structure
+
+writer.Voltage  = VRecord;
+writer.time =  millis();
+
+  
+  myfile.write((const uint8_t *)writer, sizeof(writer));
+  myfile.close();
 
 //Resetting the Counters and the Recordering Variable
 
 numRead = 0;
-ODRecord = 0;
+VRecord = 0;
 
 }
 
